bbefa8d1100d788b95a4d99990c37b5d20e0d345
Mzoon <mzoonmansour@gmail.com>
2020-12-13 16:53:30 +0300

    Update index.js

--- src/Client/index.js
+++ src/Client/index.js
@@ -2,10 +2,9 @@ import { performAction } from './j/appGeoNames'
 import { postData } from './j/PostData'
 import { getWeather } from './j/getWeather'
 import { getPic } from './j/getPicture'
-import { checkValid } from './j/checkValid'
 import './style/style.scss'
 import './style/stylemini.scss'
 import vaipl from './view/img/vairpl.png';
 let homeImg=document.getElementById('picon');
 homeImg.src=vaipl;
-export { performAction, postData, getWeather, getPic, checkValid }
+export { performAction, postData, getWeather, getPic }
